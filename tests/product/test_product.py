5cbe50596f0fa7d532289f132a7f4128bfa5b356
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-12-15 15:06:14 -0300

    test: testing that Product class has the correct attributes

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,16 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui 
+    product = Product(
+        1, 'Nicotine', 'Target Corporation',
+        '2021-02-18', '2023-01-15', 'CR25 1551 4467 2549 4402 1',
+        'instrucao 1')
+
+    assert product.id == 1
+    assert product.nome_do_produto == 'Nicotine'
+    assert product.nome_da_empresa == 'Target Corporation'
+    assert product.data_de_fabricacao == '2021-02-18'
+    assert product.data_de_validade == '2023-01-15'
+    assert product.numero_de_serie == 'CR25 1551 4467 2549 4402 1'
+    assert product.instrucoes_de_armazenamento == 'instrucao 1'
