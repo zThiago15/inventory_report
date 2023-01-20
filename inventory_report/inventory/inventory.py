e5215e280bda72b5a591a97506298b93b8bb9568
Thiago Dias <thiaguinhodias.15@gmail.com>
2023-01-19 23:06:50 -0300

    req 5 - generating reports throung JSON

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -6,14 +7,39 @@ from inventory_report.reports.complete_report import CompleteReport
 class Inventory:
 
     @staticmethod
-    def import_data(pathCSV: str, typeReport: str):
+    def import_data(path: str, typeReport: str):
+        if 'csv' in path:
+            return Inventory.read_CSV(path, typeReport)
+        elif 'json' in path:
+            return Inventory.read_JSON(path, typeReport)
+
+    @staticmethod
+    def read_CSV(pathCSV: str, typeReport: str):
         with open(pathCSV, encoding="utf-8") as file:
             data = list(csv.DictReader(file, delimiter=",", quotechar='"'))
 
+        return Inventory.generate_report(data, typeReport)
+
+    @staticmethod
+    def read_JSON(pathJSON, typeReport: str):
+        with open(pathJSON) as file:
+            data = json.loads(file.read())
+        
+        return Inventory.generate_report(data, typeReport)
+
+    @staticmethod
+    def read_JSON(pathJSON, typeReport: str):
+        with open(pathJSON) as file:
+            data = json.loads(file.read())
+        
+        return Inventory.generate_report(data, typeReport)
+
+    @staticmethod
+    def generate_report(data: list, typeReport: str):
         if (typeReport == 'simples'):
             return SimpleReport.generate(data)
 
         return CompleteReport.generate(data)
 
 
-# print(Inventory.import_data('inventory_report/data/inventory.csv', 'completo'))
+print(Inventory.import_data('inventory_report/data/inventory.json', 'completo'))
