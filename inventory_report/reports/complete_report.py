04dcd8034dd3db094eb483794f16a55add5caf42
Thiago Dias <thiaguinhodias.15@gmail.com>
2023-01-19 22:03:21 -0300

    feat: req3 - generating a complete report version, showing the count of products of each company

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,12 +1,41 @@
-from simple_report import SimpleReport
+from inventory_report.reports.simple_report import SimpleReport
+from collections import defaultdict
 
 
 class CompleteReport(SimpleReport):
 
+    def products_per_company(list_products):
+        companies = defaultdict(int)
+
+        for product in list_products:
+            companies[product['nome_da_empresa']] += 1
+
+        return companies
+
     @staticmethod
     def generate(list_products):
-        simple_report = super().generate(list_products)
-        print(simple_report)
+        companies_count = CompleteReport.products_per_company(list_products)
+
+        output = SimpleReport.generate(list_products)
+
+        output += "\nProdutos estocados por empresa:\n"
+
+        for company, count in companies_count.items():
+            output += f"- {company}: {count}\n"
+
+        return output
 
 
-print(CompleteReport.generate())
+# data = [
+#   {
+#     "id": 1,
+#     "nome_do_produto": "MESA",
+#     "nome_da_empresa": "Forces of Nature",
+#     "data_de_fabricacao": "2022-05-04",
+#     "data_de_validade": "2023-02-09",
+#     "numero_de_serie": "FR48",
+#     "instrucoes_de_armazenamento": "Conservar ao abrigo de luz"
+#   }
+# ]
+
+# print(CompleteReport.generate(data))
