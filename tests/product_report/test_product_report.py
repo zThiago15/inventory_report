87aeb65e79af3bf8e8bc9642d71bc28cc6393642
Thiago Dias <thiaguinhodias.15@gmail.com>
2023-01-19 23:48:38 -0300

    feat: req8 - testing individual report of product

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,6 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product('1', 'farinha', 'Farinini', '01-05-2021', '02-06-2023', 'DSI2138', 'ao abrigo de luz')
+    assert product.__repr__() == 'O produto farinha fabricado em 01-05-2021 por Farinini com validade até 02-06-2023 precisa ser armazenado ao abrigo de luz.'
