82ac751576b454dbcf9c1d7cd41797dfc27a8a2f
Thiago Dias <thiaguinhodias.15@gmail.com>
2023-01-20 17:11:47 -0300

    feat: req11 - generating reports throung command-line in function main

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,6 +1,19 @@
-# from sys import argv
+import sys
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
 
 
 def main():
-    def strderr():
-        pass
+    if len(sys.argv) < 3:
+        return 'Verifique os argumentos'
+
+    *_, path, report_type = sys.argv
+
+    if ('csv' in path):
+        return InventoryRefactor(CsvImporter).import_data(path, report_type)
+    elif ('json' in path):
+        return InventoryRefactor(JsonImporter).import_data(path, report_type)
+    else:
+        return InventoryRefactor(XmlImporter).import_data(path, report_type)
