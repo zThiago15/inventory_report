db1d6f1e98072e23100ec758f4aca017af09161d
Thiago Dias <thiaguinhodias.15@gmail.com>
2023-01-19 23:51:57 -0300

    fix: flake

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -2,5 +2,11 @@ from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    product = Product('1', 'farinha', 'Farinini', '01-05-2021', '02-06-2023', 'DSI2138', 'ao abrigo de luz')
-    assert product.__repr__() == 'O produto farinha fabricado em 01-05-2021 por Farinini com validade até 02-06-2023 precisa ser armazenado ao abrigo de luz.'
+    product = Product(
+        '1', 'farinha', 'Farinini', '01-05-2021', '02-06-2023', 
+        'DSI2138', 'ao abrigo de luz'
+    )
+    assert product.__repr__() == (
+        "O produto farinha fabricado em 01-05-2021 por Farinini com " +
+        "validade até 02-06-2023 precisa ser armazenado ao abrigo de luz."
+    )
